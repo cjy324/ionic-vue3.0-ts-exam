fe5e50efd0193b8e16f1fe0bb856a76c2f9df73d
cjy324 <juy32400@naver.com>
2021-03-25 22:24:45 +0900

    21.03.25 버튼 아이콘 디자인 건드려보기

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -12,17 +12,73 @@
         </ion-toolbar>
       </ion-header>
     
-      <ExploreContainer name="Tab 1 page" />
+      <ExploreContainer name="TEST Tab 1 page" />
+      <ion-button @click="presentActionSheet" expand="block" size="small">Show Action Sheet</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
+import { IonButton, actionSheetController } from '@ionic/vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+import { caretForwardCircle, close, heart, trash, share } from 'ionicons/icons';
 
 export default  {
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton },
+  setup(){
+    async function presentActionSheet() {
+      const actionSheet = await actionSheetController
+        .create({
+          header: 'Albums',
+          cssClass: 'my-custom-class',
+          buttons: [
+            {
+              text: 'Delete',
+              role: 'destructive',
+              icon: trash,
+              handler: () => {
+                console.log('Delete clicked')
+              },
+            },
+            {
+              text: 'Share',
+              icon: share,
+              handler: () => {
+                console.log('Share clicked')
+              },
+            },
+            {
+              text: 'Play (open modal)',
+              icon: caretForwardCircle,
+              handler: () => {
+                console.log('Play clicked')
+              },
+            },
+            {
+              text: 'Favorite',
+              icon: heart,
+              handler: () => {
+                console.log('Favorite clicked')
+              },
+            },
+            {
+              text: 'Cancel',
+              icon: close,
+              role: 'cancel',
+              handler: () => {
+                console.log('Cancel clicked')
+              },
+            },
+          ],
+        });
+      return actionSheet.present();
+    }
+
+    return {
+      presentActionSheet
+    }
+  }
 }
 </script>
